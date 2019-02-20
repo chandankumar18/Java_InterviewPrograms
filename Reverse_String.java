15430392b9faa94ba0b4c58aab92be4bde544ee6
chandankumar18 <chandan.kumar1990@yahoo.com>
2019-02-20 17:29:27 +0530

    Java program to reverse the String -- 3 ways

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,28 +1,45 @@
 package string_Practice;
 
+/*
+ * Java Program To to reverse the String by three ways.
+ */
+
 import java.util.Scanner;
 
 public class Reverse_String {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Please enter a String");
-		
+
 		Scanner sc = new Scanner(System.in);
 		String inputString = sc.nextLine();
-		
-		//One way
-		System.out.println("Entered String are: "+inputString);
-		System.out.println("Reverse of entered String are: "+new StringBuffer(inputString).reverse());
-		
+
+		// One way
+		System.out.println("Entered String are: " + inputString);
+		System.out.println("Using StringBuffer Class: " + new StringBuffer(inputString).reverse());
+
 		// Second way
 		String rev = "";
-		for(int i=inputString.length()-1; i>=0; i--) {
+		for (int i = inputString.length() - 1; i >= 0; i--) {
 			char ch = inputString.charAt(i);
 			rev = rev + ch;
 		}
-		System.out.println(rev);
+		System.out.println("Using iterative method: " + rev);
+
+		// Third Way
+		System.out.println("Using Recursive Method: " + recursiveMethod(inputString));
+
 		sc.close();
+	}
+
+	// Third Way
+	public static String recursiveMethod(String str) {
+		if ((null == str) || (str.length() <= 1))
+			return str;
+
+		return recursiveMethod(str.substring(1)) + str.charAt(0);
+
 	}
 
 }
